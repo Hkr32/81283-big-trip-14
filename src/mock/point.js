a96c50613d84f40f2278d19b2f89853f094e9bba
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-04-06 14:40:33 +0300

    Сделал генератор точки

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,13 +1,68 @@
+import dayjs from 'dayjs';
+
+import { getRandomInteger } from '../utils.js';
+import { eventDestinations, eventOffers, eventTypes, eventCities, eventPhotosUrl } from '../const.js';
+
+const generateDate = () => {
+  const maxDaysGap = 2;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+
+  return dayjs().add(daysGap, 'day').toDate();
+};
+
+// Случайное описание
+const generateDestinations = () => {
+  const randomIndex = getRandomInteger(0, eventDestinations.length - 1);
+
+  return eventDestinations[randomIndex];
+};
+
+// Набор предложений
+const generateOffers = () => {
+  const randomIndex = getRandomInteger(0, eventOffers.length - 1);
+
+  return eventOffers.slice(randomIndex, eventOffers.length - 1);
+};
+
+// Тип
+const generateType = () => {
+  const eventTypesKeys = Object.keys(eventTypes);
+  const randomIndex = getRandomInteger(0, eventTypesKeys.length - 1);
+
+  return eventTypes[eventTypesKeys[randomIndex]];
+};
+
+// Город
+const generateCity = () => {
+  const randomIndex = getRandomInteger(0, eventCities.length - 1);
+
+  return eventCities[randomIndex];
+};
+
+// Фото
+const generatePhotos = () => {
+  const randomCounter = getRandomInteger(1, 5);
+
+  const photos = [];
+
+  for (let i = 0; i < randomCounter; i++) {
+    photos.push(eventPhotosUrl + getRandomInteger(10, 100));
+  }
+
+  return photos;
+};
+
 // Генератор точки
 export const generatePoint = () => {
   return {
-    basePrice: 0,
-    dateFrom: null,
-    dateTo: null,
-    destination: null,
-    id: 0,
-    isFavorite: false,
-    offers: [],
-    type: null,
+    type: generateType(),
+    city: generateCity(),
+    offers: generateOffers(),
+    destination: generateDestinations(),
+    photos: generatePhotos(),
+    basePrice: getRandomInteger(10, 20, 10),
+    dateFrom: generateDate(),
+    dateTo: generateDate(),
+    isFavorite: Boolean(getRandomInteger(0, 1)),
   };
 };
