5999e5c16063b662f2d46bb7f4031516c27db859
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-05-28 15:35:41 +0300

    Удалил неиспользуемый код

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,14 +4,6 @@ export const KeyType = {
   ESC: 'Esc',
 };
 
-// Получение случайного целого числа
-export const getRandomInteger = (min = 0, max = 1, coefficient = 1) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-
-  return (Math.floor(lower + Math.random() * (upper - lower + 1)) * coefficient);
-};
-
 // Проверка нажата ли клавиша esc
 export const isEscKey = (keyType) => {
   return (keyType === KeyType.ESCAPE || keyType === KeyType.ESC);
